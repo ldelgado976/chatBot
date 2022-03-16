bbf57d6e26078f628efb1f155a82ef77e25cb964
ldelgado976 <aa21.ldelgado55@iespabloserrano.com>
2022-03-16 21:42:02 +0100

    prueba

--- 01-actions/actions/actions.py
+++ 01-actions/actions/actions.py
@@ -1,4 +1,5 @@
 import datetime as dt 
+import requests as rq
 from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
@@ -14,6 +15,22 @@ class ActionHelloWorld(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        dispatcher.utter_message(text=f"{dt.datetime.now()}")
+        dispatcher.utter_message(text=f"La hora es: {dt.datetime.now()}")
+
+        return []
+
+
+class ActionApi(Action):
+
+    def name(self) -> Text:
+        return "action_api"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        x = rq.get('https://www.zaragoza.es/sede/servicio/monumento.geojson?srsname=wgs84&rows=1&fl=id,title,description,geometry');
+        
+
+        dispatcher.utter_message(text=f"El monumento es: "+str(x.json()['features'][0]['properties']['title']))
 
         return []
